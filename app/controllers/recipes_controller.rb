87884c3b9ae0a80c0c23b3eef0cf19cbbc1232e9
CleverlyCodes <cleverly.codes@gmail.com>
2022-05-14 22:16:39 +0800

    add post action for adding recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,4 +6,23 @@ class RecipesController < ApplicationController
     def show
         @recipe = Recipe.find(params[:id])
     end
+
+    def new
+        @recipe = Recipe.new
+    end
+
+    def create
+        @recipe = Recipe.new(recipe_params)
+
+        if @recipe.save
+            @recipes = Recipe.all
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    private
+        def recipe_params
+            params.require(:recipe).permit(:name, :description)
+        end
 end
