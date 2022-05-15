0cb04d2dbbca6a529f828ae8749c5af08d0776a2
CleverlyCodes <cleverly.codes@gmail.com>
2022-05-15 23:51:03 +0800

    add functionality for adding ingredients

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -6,4 +6,21 @@ class IngredientsController < ApplicationController
   def show
     @ingredient = Ingredient.find(params[:id])
   end
+
+  def new
+    @ingredient = Ingredient.new
+  end
+
+  def create
+    @ingredient = Ingredient.new(ingredient_params)
+
+    unless @ingredient.save
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def ingredient_params
+        params.require(:ingredient).permit(:name, :description)
+    end
 end
