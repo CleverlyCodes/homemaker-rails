4f817875f0944d76c262f3b9a1eae7283840d598
mens-rea <juan.miguel.g.alvarez@gmail.com>
2022-07-20 15:02:38 +0800

    add delete function

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,11 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all().order(expired_by: :asc)
   end
+
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+
+    redirect_back_or_to :back
+  end
 end
