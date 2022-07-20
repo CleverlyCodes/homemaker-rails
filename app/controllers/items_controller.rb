c35ebecdf4a844fc272baa79a2ed1993b4db35b5
mens-rea <juan.miguel.g.alvarez@gmail.com>
2022-07-20 16:00:57 +0800

    add saving of stock items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,10 +3,39 @@ class ItemsController < ApplicationController
     @items = Item.all().order(expired_by: :asc)
   end
 
+  def create
+    print "!!!Create"
+
+    @ingredient = Ingredient.find(params[:item][:ingredient])
+
+    @bought_by = params[:item][:bought_by] ? params[:item][:bought_by] : DateTime.now
+    @production_date = params[:item][:produced_by] ? params[:item][:produced_by] : DateTime.now
+
+    @item = Item.new(
+      :ingredient => @ingredient, 
+      :quantity => params[:item][:quantity].to_i,
+      :unit => params[:item][:unit],
+      :expired_by => params[:item][:expired_by],
+      :production_date => @production_date,
+      :bought_by => @bought_by
+    )
+
+    unless @item.save
+        render :new, status: :unprocessable_entity
+    end
+
+    redirect_back_or_to :back
+  end
+
   def destroy
     @item = Item.find(params[:id])
     @item.destroy
 
     redirect_back_or_to :back
   end
+
+  private
+    def item_params
+        params.require(:item).permit(:ingredient, :quantity, :unit, :expired_by, :produced_by, :bought_by)
+    end
 end
